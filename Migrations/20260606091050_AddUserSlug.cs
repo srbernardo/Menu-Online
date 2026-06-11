e8a1154b57d3d08ab1d81fe77f69ade1352aa301
Bernardo Brotelli <b.brotelle@gmail.com>
2026-06-11 17:36:44 +0100

    Add Slug column to Users table with unique index and default value

---
+++ Migrations/20260606091050_AddUserSlug.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MenuOnline.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserSlug : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Slug",
+                table: "Users",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.Sql(
+                "UPDATE \"Users\" SET \"Slug\" = 'temp-' || \"Id\"::text " +
+                "WHERE \"Slug\" IS NULL OR \"Slug\" = '';");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Slug",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Slug",
+                table: "Users",
+                column: "Slug",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Users_Slug",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Slug",
+                table: "Users",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: false);
+
+            migrationBuilder.DropColumn(
+                name: "Slug",
+                table: "Users");
+        }
+    }
+}
